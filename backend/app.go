869d5e510ede9f2d27bb20b27a0ec1975f147f35
Lifecodev <lifecodev@mail.ru>
2026-08-16 18:32:02 +0000

    fix: fixed lint errors

--- backend/app.go
+++ backend/app.go
@@ -36,23 +36,26 @@ func (a *App) Startup(ctx context.Context) {
 	a.ctx = ctx
 	db, _ := gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
 	a.db = db 
-	a.db.AutoMigrate(&User{})
+	err := a.db.AutoMigrate(&User{})
+	if err != nil {
+    	log.Fatalf("Не удалось применить миграцию базы данных: %v", err)
+	}
 }
 
 func (a *App) Register(login string, password string) (string, error) {
 	if login == "" || password == "" {
-		return "", errors.New("Логин и пароль не могут быть пустыми")
+		return "", errors.New("логин и пароль не могут быть пустыми")
 	}
 
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return "", errors.New("Ошибка шифрования пароля")
+		return "", errors.New("ошибка шифрования пароля")
 	}
 
 	user := User{Login: login, PasswordHash: string(hashedPassword)}
 	result := a.db.Create(&user)
 	if result.Error != nil {
-		return "", errors.New("Пользователь с таким логином уже существует")
+		return "", errors.New("пользователь с таким логином уже существует")
 	}
 	return "Регистрация успешна!", nil
 }
@@ -61,12 +64,12 @@ func (a *App) Login(login string, password string) (*User, error) {
 	var user User
 	result := a.db.Where("login = ?", login).First(&user)
 	if result.Error != nil {
-		return nil, errors.New("Неверный логин или пароль")
+		return nil, errors.New("неверный логин или пароль")
 	}
 
 	err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
 	if err != nil {
-		return nil, errors.New("Неверный логин или пароль")
+		return nil, errors.New("неверный логин или пароль")
 	}
 
 	a.currentUser = &user
@@ -75,7 +78,7 @@ func (a *App) Login(login string, password string) (*User, error) {
 
 func (a *App) GetMe() (*User, error) {
 	if a.currentUser == nil {
-		return nil, errors.New("Вы не авторизованы")
+		return nil, errors.New("вы не авторизованы")
 	}
 	return a.currentUser, nil
 }
